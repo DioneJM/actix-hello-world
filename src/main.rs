0dada4d1f7bde603e5b2ae50edeb48f32f6a4b29
Dione <dionemorales@outlook.com>
2021-01-30 22:48:18 +1100

    Port change

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod hello;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let ip: String = String::from("127.0.0.1:8080");
+    let ip: String = String::from("127.0.0.1:8081");
     println!("Running on: {}", ip);
     HttpServer::new(|| App::new().service(hello::get_hello).service(hello::echo))
         .bind(ip)?
